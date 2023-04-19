18a2a7953cafafd88b2d7832e35045e56f78539f
ecai <caichengxuan.ccx@alibaba-inc.com>
2023-04-19 09:59:55 +0800

    引入viewQueue

--- app/src/main/java/com/example/test/view/CustomedLayout.java
+++ app/src/main/java/com/example/test/view/CustomedLayout.java
@@ -17,6 +17,9 @@ import androidx.annotation.Nullable;
 import com.example.test.R;
 import com.example.test.danmaku.ViweAndAnimation;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * 作者：蔡承轩（阿蔡）
  * 时间：2023/4/16 09:05
@@ -27,11 +30,7 @@ public class CustomedLayout extends ViewGroup {
 
     public static final String TAG = "CustomedLayout";
 
-//    ValueAnimator valueAnimator = ValueAnimator.ofFloat(1.0f);
-
-    private View childView;
-
-    private boolean blockShow = false;
+    private LinkedList<View> viewQueue = new LinkedList<>();
 
     public CustomedLayout(Context context) {
         super(context);
@@ -51,17 +50,12 @@ public class CustomedLayout extends ViewGroup {
         @Override
         public void onLayoutChange(View v, int left, int top, int right, int bottom, int oldLeft, int oldTop, int oldRight, int oldBottom) {
             if (MeasureSpec.getSize(getMeasuredWidth()) - right > minGap){
-                blockShow = false;
-                index++;
-                start();
                 Log.e(TAG, "释放下一条弹幕: ");
+                v.removeOnLayoutChangeListener(this);
                 showNext();
             }
         }
     };
-
-    private void showNext() {
-    }
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
@@ -81,8 +75,8 @@ public class CustomedLayout extends ViewGroup {
         Log.e(TAG, "left: " + l +" top: " + t + " right: " + r + " bottom: " + b);
         for (int i=0; i<childCount;i++){
             View childView = getChildAt(i);
+            viewQueue.addLast(childView);
             childView.addOnLayoutChangeListener(layoutChangeListener);
-
             int childWidth = childView.getMeasuredWidth();
             int childHeight = childView.getMeasuredHeight();
             Log.e(TAG, "childWidth: " + MeasureSpec.getSize(childWidth) + " childHeight: " +
@@ -93,57 +87,38 @@ public class CustomedLayout extends ViewGroup {
             heightOffset += childView.getMeasuredHeight();
         }
     }
-    int index = 0;
     public void start() {
         Log.e(TAG, "start: ");
-        if (blockShow) return;
-        int childCount = getChildCount();
-//        valueAnimator.setDuration(2000);
-//        valueAnimator.setInterpolator(new LinearInterpolator());
-        if (index >=3)return;
-//        for (int i=0; i<childCount; i++){
         ViweAndAnimation viewAndAnimation = new ViweAndAnimation(getMeasuredWidth());
         if (viewAndAnimation.childView != null) {
             viewAndAnimation.childView.removeOnLayoutChangeListener(layoutChangeListener);
         }
-        viewAndAnimation.childView = getChildAt(index);
+        if (viewQueue.isEmpty()) return;
+        viewAndAnimation.childView = viewQueue.poll();
         viewAndAnimation.childView.addOnLayoutChangeListener(layoutChangeListener);
-//            valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
-//                @Override
-//                public void onAnimationUpdate(ValueAnimator animation) {
-//                    float animatedFraction = animation.getAnimatedFraction();
-//                    int left = (int) (getMeasuredWidth() - animatedFraction * (getMeasuredWidth()
-//                            + viewAndAnimation.childView.getMeasuredWidth()));
-//
-//                    viewAndAnimation.childView.layout(left, viewAndAnimation.childView.getTop(),
-//                            left + viewAndAnimation.childView.getMeasuredWidth(),
-//                            childView.getTop() + childView.getMeasuredHeight());
-//                }
-//            });
-//        }
-//        valueAnimator.start();
         viewAndAnimation.start();
-        blockShow = true;
+    }
+
+    /**
+     * 展示下一条弹幕
+     */
+    private void showNext() {
+        if (viewQueue.isEmpty()) return;
+        ViweAndAnimation viweAndAnimation = new ViweAndAnimation(getMeasuredWidth());
+        viweAndAnimation.childView = viewQueue.poll();
+        viweAndAnimation.childView.addOnLayoutChangeListener(layoutChangeListener);
+        viweAndAnimation.start();
     }
 
     public void stop() {
         Log.e(TAG, "pause: ");
-//        if (valueAnimator.isStarted()){
-//            valueAnimator.cancel();
-//        }
     }
 
     public void pause() {
         Log.e(TAG, "pause: ");
-//        if (valueAnimator.isRunning()){
-//            valueAnimator.pause();
-//        }
     }
 
     public void resume() {
         Log.e(TAG, "pause: ");
-//        if (valueAnimator.isPaused()){
-//            valueAnimator.resume();
-//        }
     }
 }
