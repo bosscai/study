c3227c85500d7fae78130518991634427cb1c129
ecai <caichengxuan.ccx@alibaba-inc.com>
2023-04-19 11:50:08 +0800

    手动添加View

--- app/src/main/java/com/example/test/view/CustomedLayout.java
+++ app/src/main/java/com/example/test/view/CustomedLayout.java
@@ -5,12 +5,14 @@ import android.util.AttributeSet;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 
 import com.example.test.danmaku.ViweAndAnimation;
 
 import java.util.LinkedList;
+import java.util.Random;
 
 /**
  * 作者：蔡承轩（阿蔡）
@@ -41,8 +43,7 @@ public class CustomedLayout extends ViewGroup {
     private OnLayoutChangeListener layoutChangeListener = new OnLayoutChangeListener() {
         @Override
         public void onLayoutChange(View v, int left, int top, int right, int bottom, int oldLeft, int oldTop, int oldRight, int oldBottom) {
-            if (MeasureSpec.getSize(getMeasuredWidth()) - right > minGap){
-                Log.e(TAG, "释放下一条弹幕: ");
+            if (MeasureSpec.getSize(getMeasuredWidth()) - right > minGap) {
                 v.removeOnLayoutChangeListener(this);
                 showNext();
             }
@@ -54,13 +55,9 @@ public class CustomedLayout extends ViewGroup {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
         int widthSize = MeasureSpec.getSize(widthMeasureSpec);
         int heightSize = MeasureSpec.getSize(heightMeasureSpec);
-        Log.e(TAG, "onMeasure widthSize: " +  widthSize + " heightSize:" + heightSize);
+        Log.e(TAG, "onMeasure widthSize: " + widthSize + " heightSize:" + heightSize);
         //测量子布局
         measureChildren(widthMeasureSpec, heightMeasureSpec);
-        for (int i=0; i<getChildCount();i++){
-            View childView = getChildAt(i);
-            viewQueue.addLast(childView);
-        }
     }
 
     @Override
@@ -68,17 +65,36 @@ public class CustomedLayout extends ViewGroup {
         int childCount = getChildCount();
         int heightOffset = 0;
         Log.e(TAG, "childCount: " + childCount);
-        Log.e(TAG, "left: " + l +" top: " + t + " right: " + r + " bottom: " + b);
+        Log.e(TAG, "left: " + l + " top: " + t + " right: " + r + " bottom: " + b);
     }
+
     public void start() {
         Log.e(TAG, "start: ");
+        for (int i = 0; i < 10; i++) {
+            TextView textView = new TextView(getContext());
+            textView.setText(getRandomString(10));
+            viewQueue.addLast(textView);
+            addView(textView);
+        }
         showNext();
+    }
+
+    public String getRandomString(int length) {
+        String str = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        Random random = new Random();
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0; i < length; i++) {
+            int number = random.nextInt(62);
+            sb.append(str.charAt(number));
+        }
+        return sb.toString();
     }
 
     /**
      * 展示下一条弹幕
      */
     private void showNext() {
+        Log.e(TAG, "showNext: viewQueue.isEmpty: " + viewQueue.isEmpty());
         if (viewQueue.isEmpty()) return;
         ViweAndAnimation viweAndAnimation = new ViweAndAnimation(getMeasuredWidth());
         viweAndAnimation.childView = viewQueue.poll();
