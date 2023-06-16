56555d789438fa71014b290f72867eae8ec07954
ecai <caichengxuan.ccx@alibaba-inc.com>
2023-06-16 09:53:18 +0800

    study action

---
+++ app/src/main/java/com/example/test/ActionStudyActivity.java
@@ -0,0 +1,14 @@
+package com.example.test;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActionStudyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_action_study);
+    }
+}
