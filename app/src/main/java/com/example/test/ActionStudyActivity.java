261e9619fb4faf45be0d5cd60eae042431f9bca5
ecai <caichengxuan.ccx@alibaba-inc.com>
2023-07-06 18:09:03 +0800

    震动

--- app/src/main/java/com/example/test/ActionStudyActivity.java
+++ app/src/main/java/com/example/test/ActionStudyActivity.java
@@ -2,14 +2,20 @@ package com.example.test;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Service;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Vibrator;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class ActionStudyActivity extends AppCompatActivity {
 
     private static final String TAG = "ActionStudyActivity";
+
+    private Button mBtnStartVibrator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,5 +24,14 @@ public class ActionStudyActivity extends AppCompatActivity {
         Intent intent = getIntent();
         Uri data = intent.getData();
         Log.e(TAG, "onCreate: " + data );
+
+        mBtnStartVibrator = findViewById(R.id.btn_start_vibrator);
+        mBtnStartVibrator.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Vibrator service = (Vibrator) getSystemService(Service.VIBRATOR_SERVICE);
+                service.vibrate(3000L);
+            }
+        });
     }
 }
